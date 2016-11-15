1fdb5fd3936d1a28253af2f44628e2e7631e4ece
kriakaziaba <kriakaziaba@gmail.com>
2016-11-15 18:39:49 +0200

    add some ui and add new dependency

--- doit.js
+++ doit.js
@@ -1,5 +1,26 @@
-deacribe('magia', function () {
-   it('go go little girl', function () {
+'use strict';
+var nodemailer = require('nodemailer');
 
+describe('magia', function () {
+   var transporter = nodemailer.createTransport(transport[, defaults])
+   it('go go little girl', function () {
+      browser.ignoreSynchronization = true;
+      browser.get('http://seleniumcamp.com/registration/');
+      var prom = $('div.row>div:nth-child(1)>div>div.stage-left').getText();
+      var text = prom.then(function(text) {
+         if (text === '0 left'){
+            transporter.sendMail({from : 'timur@kubay.com', to: 'luxofttest1002@gmail.com', subject: 'go and buy'});
+         }
+         else {
+            transporter.sendMail({from : 'timur@kubay.com', to: 'luxofttest1001@gmail.com', subject: 'go and buy'});
+         }
+      });
+      console.log(text === '1 left');
+      // if (text === '1 left'){
+      //    console.log('true')
+      // }
+      // else {
+      //    console.log('false')
+      // }
    })
 });
